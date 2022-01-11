1ef2c734a580538e1966a428e2a83dcf32bfb219
abdullahbasal <1abdullahbasal@gmail.com>
2022-01-11 16:44:23 +0300

    search bar added and css changed

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,40 +1,56 @@
-import React from "react";
+/* eslint-disable array-callback-return */
+import React, { useState } from "react";
 
 import "../Styles/List.css";
 
 const List = (props) => {
+  const [searchedTitle, setSearchedTitle] = useState("");
   function deletePopup(id) {
     props.deletePopup(id);
   }
-
+  console.log(searchedTitle);
   return (
     <div>
-      {props.list.map((item, index) => (
-        <div key={index}>
-          <ol className="list">
-            <li
-              className="item"
-              data-content={item.id}
-              onClick={() => props.onSelectedItemChange(item)}
-            >
-              <h2 className="headline">{item.data?.title}</h2>
-              <span>{item.data?.body}</span>
-            </li>
-            <div className="trash-box-container">
-              <div className="trash-box" onClick={() => deletePopup(item.id)}>
-                <div className="trash"></div>
-                <div className="trash-top"></div>
-                <div className="trash-btm">
-                  <div className="trash-lines">
-                    <div className="trash-line"></div>
-                    <div className="trash-line"></div>
+      <input
+        type="text"
+        class="icon-rtl"
+        placeholder="Arama Yapınız..."
+        onChange={(e) => setSearchedTitle(e.target.value)}
+      />
+      {props.list
+        .filter((item) => {
+          if (searchedTitle === "") {
+            return item;
+          } else if (item.data.title.toLowerCase().includes(searchedTitle)) {
+            return item;
+          }
+        })
+        .map((item, index) => (
+          <div className="card" key={index}>
+            <ol className="list">
+              <li
+                className="item"
+                data-content={item.id}
+                onClick={() => props.onSelectedItemChange(item)}
+              >
+                <h2 className="headline">{item.data?.title}</h2>
+                <span>{item.data?.body}</span>
+              </li>
+              <div className="trash-box-container">
+                <div className="trash-box" onClick={() => deletePopup(item.id)}>
+                  <div className="trash"></div>
+                  <div className="trash-top"></div>
+                  <div className="trash-btm">
+                    <div className="trash-lines">
+                      <div className="trash-line"></div>
+                      <div className="trash-line"></div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </ol>
-        </div>
-      ))}
+            </ol>
+          </div>
+        ))}
     </div>
   );
 };
